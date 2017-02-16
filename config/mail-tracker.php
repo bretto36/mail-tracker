88afcae44ca93c35b388ee99c74c57e5e94ceea9
Brett B <bretto36@hotmail.com>
2017-02-16 11:02:02 +1000

    Updated spelling mistakes

--- config/mail-tracker.php
+++ config/mail-tracker.php
@@ -4,17 +4,17 @@ return [
     /**
      * To disable the pixel injection, set this to false.
      */
-    'inject-pixel'=>true,
+    'inject-pixel' => true,
 
     /**
      * To disable injecting tracking links, set this to false.
      */
-    'track-links'=>true,
+    'track-links' => true,
 
     /**
      * Optionally expire old emails, set to 0 to keep forever.
      */
-    'expire-days'=>60,
+    'expire-days' => 60,
 
     /**
      * Where should the pingback URL route be?
@@ -33,21 +33,21 @@ return [
     ],
 
     /**
-     * Admin Tamplate
+     * Admin Template
      * example
-     * 'name' => 'layouts.app' for Default emailTraking use 'emailTrakingViews::layouts.app'
-     * 'section' => 'content' for Default emailTraking use 'content'
-     * 'styles_section' => 'styles' for Default emailTraking use 'styles'
+     * 'name' => 'layouts.app' for Default emailTracking use 'emailTrackingViews::layouts.app'
+     * 'section' => 'content' for Default emailTracking use 'content'
+     * 'styles_section' => 'styles' for Default emailTracking use 'styles'
      */
     'admin-template' => [
-        'name' => 'emailTrakingViews::layouts.app',
+        'name' => 'emailTrackingViews::layouts.app',
         'section' => 'content',
     ],
 
     /**
      * Number of emails per page in the admin view
      */
-    'emails-per-page'=>30,
+    'emails-per-page' => 30,
 
     /**
      * Date Format
